a0c5a57b210dd4683c4749522c958d576cd4af83
danielvazquez-utp <daniel.vazquez@utpuebla.edu.mx>
2025-03-07 12:23:25 -0600

    Lista desplegable agregada a formulario de artículos

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,12 +1,231 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import { nameValidator } from '../helpers/nameValidator'
 
 
-import { View, Text } from 'react-native'
-import React from 'react'
+import SelectDropdown from 'react-native-select-dropdown'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export const ArticleScreen = () => {
+import { useFetch } from '../hooks/useFetch';
+
+export function ArticleScreen({ navigation }) {
+  const [name, setName] = useState({ value: '', error: '' })
+  const [lastName, setLastName] = useState({ value: '', error: '' })
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+
+  const itemTypes = [
+    {title:'Mobiliario de oficina'},
+    {title:'Equipo de cómputo'},
+    {title:'Otros'},
+  ];
+
+  const itemState = [
+    {title:'Sin determinar'},
+    {title:'Mal estado'},
+    {title:'Regular'},
+    {title:'Buen estado'},
+    {title:'Excelente estado'},
+  ]
+
+  const locations = [
+    {title:'Almacen'},
+    {title:'Recepción'},
+  ]
+
+  const { getData, setData } = useFetch();
+
+  const onSignUpPressed = async() => {
+    const nameError = nameValidator(name.value)
+    const lastNameError = nameValidator(lastName.value)
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
+    if (emailError || lastNameError || passwordError || nameError) {
+      setName({ ...name, error: nameError })
+      setLastName({ ...name, error: lastNameError })
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
+      return
+    }
+
+    const usuario = await getData('http://localhost:3000/api/users/byNick/' + email.value);
+    if (usuario.error) return;
+    const { data } = usuario;
+    if( data.length>0 ) return;
+
+    const nuevoUsuario = {
+      name: name.value,
+      lastName: lastName.value,
+      nickname: email.value,
+      password: password.value,
+      profile: 1,
+      state: 1
+    }
+
+    const nuevo = await setData('http://localhost:3000/api/users/add', nuevoUsuario );
+    if (nuevo.error) return;
+    
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Dashboard' }],
+    })
+  }
+
   return (
-    <View>
-      <Text>ArticleScreen</Text>
-    </View>
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Agregar artículo</Header>
+      <TextInput
+        label="Nombre"
+        returnKeyType="next"
+        value={name.value}
+        onChangeText={(text) => setName({ value: text, error: '' })}
+        error={!!name.error}
+        errorText={name.error}
+      />
+
+      <SelectDropdown
+        data={ itemTypes }
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        renderButton={(selectedItem, isOpened) => {
+          return (
+            <View style={styles.dropdownButtonStyle}>
+              <Text style={styles.dropdownButtonTxtStyle}>
+                {(selectedItem && selectedItem.title) || 'Seleccionar tipo de artículo'}
+              </Text>
+
+              <Ionicons 
+                name={isOpened ? 'chevron-up' : 'chevron-down'} 
+                size={24} 
+                color="black" 
+                style={styles.dropdownButtonArrowStyle}
+              />
+
+            </View>
+          );
+        }}
+        renderItem={(item, index, isSelected) => {
+          return (
+            <View style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
+              <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+            </View>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+        dropdownStyle={styles.dropdownMenuStyle}
+      />
+
+      <TextInput
+        label="Apellidos"
+        returnKeyType="next"
+        value={lastName.value}
+        onChangeText={(text) => setLastName({ value: text, error: '' })}
+        error={!!lastName.error}
+        errorText={lastName.error}
+      />
+      <TextInput
+        label="Correo"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Contraseña"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <Button
+        mode="contained"
+        onPress={onSignUpPressed}
+        style={{ marginTop: 24 }}
+      >
+        Sign Up
+      </Button>
+      <View style={styles.row}>
+        <Text>Already have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+          <Text style={styles.link}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+  dropdownButtonStyle: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#E9ECEF',
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+  },
+  dropdownButtonTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+  },
+  dropdownButtonArrowStyle: {
+    fontSize: 28,
+  },
+  dropdownButtonIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+  },
+  dropdownMenuStyle: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+  },
+  dropdownItemStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+  },
+  dropdownItemIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+  },
+})
